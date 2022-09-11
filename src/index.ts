27d37012467cd9a4d8d217687fb9ee81ec453d00
Rob van Bakel <robbakel@gmail.com>
2022-09-11 11:42:32 +0200

    Use cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 import redirectCode from "./routes/redirectCode";
