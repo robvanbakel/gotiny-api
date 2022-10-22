fc4deed2480e5c6512e78d49022df971649dec7f
Rob van Bakel <robbakel@gmail.com>
2022-10-22 10:30:00 +0200

    Refactor getTiny function

--- src/utils/getTiny.ts
+++ src/utils/getTiny.ts
@@ -1,11 +1,10 @@
 const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'k', 'm', 'n', 'p', 'r', 's', 't', 'w', 'x', 'y', 'z', '2', '3', '4', '5', '6', '7', '8'];
 
 export default (len: number) => {
-  let id = '';
 
-  for (let i = 0; i < len; i += 1) {
-    id += chars[Math.floor(Math.random() * chars.length)];
-  };
-
-  return id;
+  return Array(len)
+    .fill(true)
+    .map(() => chars[Math.floor(Math.random() * chars.length)])
+    .join('')
+  
 };
